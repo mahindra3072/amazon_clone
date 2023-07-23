d53f6ac01fa3f2275b658521196f9e2c069a987b
mahindra <kodalimahindra3072@gmail.com>
2023-07-23 21:20:30 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBvb1oCOhNjTLjEcHi3_tkb1lnVwQJzcOE",
+    authDomain: "clone-337ff.firebaseapp.com",
+    projectId: "clone-337ff",
+    storageBucket: "clone-337ff.appspot.com",
+    messagingSenderId: "691200077516",
+    appId: "1:691200077516:web:9191f2ecb245c29bd35c69"
+  };
