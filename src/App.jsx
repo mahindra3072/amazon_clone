5ebf74d6073067df5c2d94daae3c8c4a3f3564ad
temporeader <72514187+mahindra3072@users.noreply.github.com>
2023-07-24 09:07:32 +0000

    Update 7 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
-import "./App.css"
+import "./App.css";
+import Checkout from "./Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App(){
-    return(
-        <div className="app">
+function App() {
+  return (
+    <Router>
+      <div className="app">
         <Header />
-        <Home/>
-        </div>
-    )
+        <Routes>
+          <Route path="/" element={[<Home />]} />
+          <Route path="/checkout" element={[<Checkout />]} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import './Header.css';
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import amazon_logo from "./pictures/amazon_PNG11.png";
 
 function Header() {
   return (
-    <div className='header'>
-    <img className='header_logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='amazon logo'/>
-    <div className='header_search'>
-        <input className='header_searchInput' type="text"/>
-        <SearchIcon className="header_searchIcon"/>
+    <div className="header">
+      <img className="header_logo" src={amazon_logo} alt="amazon logo" />
+      <div className="header_search">
+        <input className="header_searchInput" type="text" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionOne">Hello Guest</span>
+          <span className="header_optionTwo">Sign In</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionOne">Returns</span>
+          <span className="header_optionTwo">& Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionOne">Your</span>
+          <span className="header_optionTwo">Prime</span>
+        </div>
+        <div className="header_optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header_optionOne header_basketCount">0</span>
+        </div>
+      </div>
     </div>
-    <div className='header_nav'>
-     <div className='header_option'>
-        <span className='header_optionOne'>Hello Guest</span>
-        <span className='header_optionTwo'>Sign In</span>
-     </div>
-     <div className='header_option'>
-        <span className='header_optionOne'>Returns</span>
-        <span className='header_optionTwo'>& Orders</span>
-     </div>
-     <div className='header_option'>
-        <span className='header_optionOne'>Your</span>
-        <span className='header_optionTwo'>Prime</span>
-     </div>
-     <div className='header_optionBasket'>
-        <ShoppingBasketIcon/>
-        <span className='header_optionOne header_basketCount'>0</span>
-     </div>
-
-    </div>
-    </div>
-  )
+  );
 }
 
 export default Header;
